67848d28de99ab54fa26b271ccedaace235e5b5a
Yumi Izumi <me@yumiizumi.com>
2024-02-19 16:43:03 -0500

    refactor(tests): put tests in #[cfg(test)]

--- tests/parser_tests.rs
+++ tests/parser_tests.rs
@@ -1,59 +1,60 @@
-use html2md_rs::{
-    parser::*,
-    structs::{
-        Node,
-        NodeType::{self, *},
-    },
-};
+#[cfg(test)]
 
-#[test]
-fn parse_simple_div_with_text() {
-    let input = "<div>hello</div>".to_string();
-    let expected = Node {
-        tag_name: Some(Div),
-        value: None,
-        attributes: None,
-        children: vec![Node {
-            tag_name: Some(Text),
-            value: Some("hello".to_string()),
+mod parser_tests {
+    use html2md_rs::{
+        parser::*,
+        structs::{Node, NodeType::*},
+    };
+
+    #[test]
+    fn parse_simple_div_with_text() {
+        let input = "<div>hello</div>".to_string();
+        let expected = Node {
+            tag_name: Some(Div),
+            value: None,
             attributes: None,
-            children: vec![],
-        }],
-    };
-    assert_eq!(parse_html(input), expected);
-}
+            children: vec![Node {
+                tag_name: Some(Text),
+                value: Some("hello".to_string()),
+                attributes: None,
+                children: vec![],
+            }],
+        };
+        assert_eq!(parse_html(input), expected);
+    }
 
-#[test]
-fn parse_multiple_headers() {
-    let input = "<h1>hello</h1><h2>world</h2>".to_string();
-    let expected = Node {
-        tag_name: None,
-        value: None,
-        attributes: None,
-        children: vec![
-            Node {
-                tag_name: Some(H1),
-                value: None,
-                attributes: None,
-                children: vec![Node {
-                    tag_name: Some(Text),
-                    value: Some("hello".to_string()),
+    #[test]
+    fn parse_multiple_headers() {
+        let input = "<h1>hello</h1><h2>world</h2>".to_string();
+        let expected = Node {
+            tag_name: None,
+            value: None,
+            attributes: None,
+            children: vec![
+                Node {
+                    tag_name: Some(H1),
+                    value: None,
                     attributes: None,
-                    children: vec![],
-                }],
-            },
-            Node {
-                tag_name: Some(H2),
-                value: None,
-                attributes: None,
-                children: vec![Node {
-                    tag_name: Some(Text),
-                    value: Some("world".to_string()),
+                    children: vec![Node {
+                        tag_name: Some(Text),
+                        value: Some("hello".to_string()),
+                        attributes: None,
+                        children: vec![],
+                    }],
+                },
+                Node {
+                    tag_name: Some(H2),
+                    value: None,
                     attributes: None,
-                    children: vec![],
-                }],
-            },
-        ],
-    };
-    assert_eq!(parse_html(input), expected);
+                    children: vec![Node {
+                        tag_name: Some(Text),
+                        value: Some("world".to_string()),
+                        attributes: None,
+                        children: vec![],
+                    }],
+                },
+            ],
+        };
+        assert_eq!(parse_html(input), expected);
+    }
 }
