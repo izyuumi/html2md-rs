1f55dceb56b4e6ca5647f489317f5d686f27e1d8
Yumi Izumi <me@yumiizumi.com>
2024-02-21 18:19:46 -0500

    fix: empty paragraphs should not return anything

--- src/to_md.rs
+++ src/to_md.rs
@@ -49,6 +49,12 @@ pub fn to_md(node: Node) -> String {
                 tail.push_str("\n");
             }
             P => {
+                if node.children.len() == 1
+                    && node.children[0].tag_name == Some(Text)
+                    && node.children[0].value.is_none()
+                {
+                    return res;
+                }
                 tail.push_str("\n\n");
             }
             Text => {
